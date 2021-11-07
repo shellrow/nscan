dd9a3eb31aa0e4507a2a32277af8e7f315e9fc1b
shellrow <81893184+shellrow@users.noreply.github.com>
2021-11-07 13:12:02 +0900

    Update define.rs

--- src/define.rs
+++ src/define.rs
@@ -1,4 +1,4 @@
-pub const CRATE_UPDATE_DATE: &str = "2021-09-26";
+pub const CRATE_UPDATE_DATE: &str = "2021-11-06";
 pub const CRATE_AUTHOR_GITHUB: &str = "shellrow <https://github.com/shellrow>";
 pub const CRATE_REPOSITORY: &str = "https://github.com/shellrow/nscan";
 pub const PORTSCAN_TYPE_SYN_SCAN: &str = "SYN";
