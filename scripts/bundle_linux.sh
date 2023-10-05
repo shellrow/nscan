b6d3c7435f06ed2001ca03691fe5950fdb525519
shellrow <81893184+shellrow@users.noreply.github.com>
2023-10-05 22:20:45 +0900

    Add bundle scripts

---
+++ scripts/bundle_linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# build and bundle the nscan binary and license files into a zip file
+# usage: 
+# chmod +x ./scripts/bundle.sh　(Only needed if not already set)
+# ./scripts/bundle.sh
+
+bin_name="nscan"
+version="0.14.0"
+os_arch="x86_64-unknown-linux-gnu"
+dist_dir="./dist"
+
+zip_filename="nscan-$version-$os_arch.zip"
+
+echo "Building nscan binary for $os_arch"
+cargo build --release
+
+# if dist_dir does not exist, create it
+if [ ! -d $dist_dir ]; then
+    mkdir $dist_dir
+fi
+
+cp ./target/release/$bin_name $dist_dir/$bin_name
+cp ./LICENSE $dist_dir/LICENSE
+
+cd $dist_dir
+echo "Creating zip file $zip_filename"
+zip -r $zip_filename $bin_name LICENSE
+echo "Done"
---
+++ scripts/bundle_win.ps1
@@ -0,0 +1,27 @@
+# build and bundle the nscan binary and license files into a zip file
+# usage: 
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process (Only needed if not already set)
+# .\scripts\bundle.ps1
+
+$binName = "nscan.exe"
+$version = "0.14.0"
+$osArch = "x86_64-pc-windows-msvc"
+$distDir = ".\dist"
+
+$zipFilename = "nscan-$version-$osArch.zip"
+
+Write-Host "Building nscan binary for $osArch"
+cargo build --release
+
+# if distDir does not exist, create it
+if (-not (Test-Path -Path $distDir -PathType Container)) {
+    New-Item -Path $distDir -ItemType Directory
+}
+
+Copy-Item -Path ".\target\release\$binName" -Destination "$distDir\$binName" -Force
+Copy-Item -Path ".\LICENSE" -Destination "$distDir\LICENSE" -Force
+
+Set-Location -Path $distDir
+Write-Host "Creating zip file $zipFilename"
+Compress-Archive -Path "$binName", "LICENSE" -DestinationPath $zipFilename
+Write-Host "Done"
